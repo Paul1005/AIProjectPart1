5696111d6a675c17d0a9074802576d3acc9695e0
Paul1005 <mccarliep@gmail.com>
2019-09-30 23:57:26 -0700

    first commit

---
+++ Assets/ShipCard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipCard : MonoBehaviour {
+	public string type;
+	public int hits;
+	public int speed;
+	public int turns;
+	public int shields;
+	public int armour;
+	public int turrets;
+    public ArrayList weapons;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
