74abdd32e3f402e71c548ea1aa43e3be1e8ff4b6
Paul1005 <mccarliep@gmail.com>
2019-10-02 22:07:15 -0700

    added more stuff

---
+++ Assets/Weapons/Scripts/WeaponCard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCard : MonoBehaviour {
+
+	public string type;
+	public int range;
+	public int firepower;
+	public string fireArc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
